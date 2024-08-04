b1633b1d5a40accd7b52fca847a6402b0ffe5556
faraz <faraz.fatahnaie@gmail.com>
2024-08-04 12:07:34 +0330

    moved to /app

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -5,9 +5,8 @@ from sqlalchemy import pool
 from alembic import context
 import os
 import sys
-sys.path.insert(0, os.path.realpath(os.path.join(os.path.dirname(__file__), '..')))
-from database import Base
-from models import *
+sys.path.insert(0, os.path.realpath(os.path.join(os.path.dirname(__file__), '../..')))
+from app.models import *
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
